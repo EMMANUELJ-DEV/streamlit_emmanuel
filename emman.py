bb47c1f0b07c272815128332b7e490f59d93efd0
EMMANUELJ-DEV <159872832+EMMANUELJ-DEV@users.noreply.github.com>
2025-07-18 08:44:15 +0530

    Update emman.py

--- emman.py
+++ emman.py
@@ -1,5 +1,9 @@
 import streamlit as st
-st.write("hello i am emmanuel")
-s=st.text_input("enter your name")
-if s:
-      st.write(f"{s},nice to meet you")
+
+st.write("Hello, I am Emmanuel")
+
+uploaded_file = st.file_uploader("Upload your file")
+
+if uploaded_file is not None:
+    st.write(f"File name: {uploaded_file.name}")
+    st.success(f"Nice to meet you! Your file '{uploaded_file.name}' has been uploaded.")
